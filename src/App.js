2ce9383bad05390fd60e02e2873ab0dbffaa1ce7
fabalvesfr <fabio9162@gmail.com>
2023-11-17 10:06:06 -0300

    first commmit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Person } from "./Components/Person";
+
+function App() {
+  return (
+    <div className="App">
+      <Person
+        name="Pedro"
+        email="pedro@gmail.com"
+        age={21}
+        isMarried={true}
+        friends={["jessica", "jake", "josh", "jasmine"]}
+      />
+    </div>
+  );
+}
+
+export default App;
